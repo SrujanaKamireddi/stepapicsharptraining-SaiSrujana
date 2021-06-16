5f133bc35e595a74187d6fc3e61653e843dd31c6
Sai Srujana Kamireddi <saisrujanakamireddi55@gmail.com>
2021-06-16 17:10:21 +0530

    HelloWorld

---
+++ C#/1.Introduction to C#/1_HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your Name: ");
+            var Name = Console.ReadLine();
+            Console.WriteLine("Hi {0} Welcome to C-Sharp :-) ",Name);
+        }
+    }
+}
