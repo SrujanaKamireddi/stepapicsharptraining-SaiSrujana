7e562057dfc89f677d0e43a71c62023b7ef83a83
Sai Srujana Kamireddi <saisrujanakamireddi55@gmail.com>
2021-06-16 17:27:20 +0530

    Print Even and Odd

---
+++ C#/1.Introduction to C#/2_EvenOddNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2_EvenOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            Console.WriteLine("Enter a Number: ");
+            int Num =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Even Numbers: ");
+
+            for(index = 0; index <= Num; index++)
+            {
+                if (index % 2 == 0)
+                {
+                    Console.WriteLine(index);
+                }
+               
+            }
+            Console.WriteLine("Odd Numbers: ");
+            for (index = 0; index <= Num; index++)
+            {
+                if (index % 2 != 0)
+                {
+                    Console.WriteLine(index);
+                }
+
+            }
+        }
+    }
+}
